8bbd7c986cd17ca0ec913eb4b02ab829bd80a944
Arkadi6777 <sargsaninga611@gmail.com>
2024-11-30 00:39:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setInterval(() => {
     // Время до конца конкурса
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60)) / (1000 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
     timer.innerHTML = `До начала конкурса: ${days}д ${hours}ч ${minutes}м ${seconds}с`;
@@ -22,7 +22,7 @@ setInterval(() => {
 
     if (timeLeft > 0) {
       const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeLeft % (1000 * 60)) / (1000 * 60));
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)); // Количество минут
       const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
       timer.innerHTML = `Конкурс завершится через: ${hours}ч ${minutes}м ${seconds}с`;
